24a2a6251954eb661aca92612d156aad86d3dbd9
ThankOwl777 <zeidgame@gmail.com>
2021-02-24 18:27:35 +0900

    docs : 1181 solve
    + 백준 알고리즘
      + 1181 단어 정렬 solve

--- boj/src/main/java/boj/Boj1181.java
+++ boj/src/main/java/boj/Boj1181.java
@@ -1,27 +1,44 @@
 package boj;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Boj1181 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Queue<String> que = new LinkedList<>();
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         String[] arr = new String[n];
         
         for(int i = 0; i < n; i++) {
-            arr[i] = sc.next();
+            arr[i] = br.readLine();
         }
 
         Arrays.sort(arr, new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
-                return s1.length() - s2.length();
+                
+                if(s1.length() == s2.length()) {
+                    return s1.compareTo(s2);
+                } else {
+                    return s1.length() - s2.length();
+                }
             }
         });
 
-        
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(arr[0]).append('\n');
+
+        for(int i = 1; i < n; i++) {
+            if(!arr[i].equals(arr[i - 1])) {
+                sb.append(arr[i]).append('\n');
+            }
+        }
+
+        System.out.println(sb);
 
     }
 }
