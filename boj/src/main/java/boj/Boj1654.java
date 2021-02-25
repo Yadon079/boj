e7502ed2b7fef4af1ea6a15654480149355822f2
ThankOwl777 <zeidgame@gmail.com>
2021-02-25 15:57:38 +0900

    docs : 1654 solve
    + 백준 알고리즘 이분탐색
      + 1654 랜선 자르기 solve

---
+++ boj/src/main/java/boj/Boj1654.java
@@ -0,0 +1,42 @@
+package boj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Boj1654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[k];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        long max = arr[k - 1]; // 제일 긴 랜선
+        long min = 1; // 랜선 길이는 자연수
+        long mid = 0;
+
+        while(max >= min) {
+            mid = (max + min) / 2;
+
+            long cnt = 0;
+
+            for(int i = 0; i < arr.length; i++) {
+                cnt += arr[i] / mid;
+            }
+
+            if(cnt >= n) {
+                min = mid + 1;
+            } else if(cnt < n) {
+                max = mid - 1;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
