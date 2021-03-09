f34c0603a80d966c320e7b586098c0e4a3c6b93f
GraceNam <zeidgame@gmail.com>
2021-03-10 00:18:51 +0900

    docs : 숫자 카드 2
    + 백준 알고리즘 10816 solved
      + HashMap을 사용한 코드

--- boj/src/main/java/boj/Boj10816.java
+++ boj/src/main/java/boj/Boj10816.java
@@ -1,17 +1,51 @@
 package boj;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Boj10816 {
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        HashMap<Integer, Integer> num = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
 
-        int n = 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            int card = Integer.parseInt(st.nextToken());
+            
+            if(map.containsKey(card)) {
+                map.replace(card, map.get(card) + 1);
+            } else {
+                map.put(card, 1);
+            }
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if(map.containsKey(num)) {
+                int cnt = map.get(num);
+
+                sb.append(cnt);
+                sb.append(" ");
+            } else {
+                sb.append(0);
+                sb.append(" ");
+            }
+        }
+
+        System.out.println(sb);
+
     }
 
     /*
