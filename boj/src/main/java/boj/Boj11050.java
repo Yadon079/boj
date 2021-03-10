1a87afcf363d04d84b4d274bcb22536e2ff9eaea
GraceNam <zeidgame@gmail.com>
2021-03-10 21:31:29 +0900

    docs : 이항 계수 1
    + 백준 알고리즘 11050 solved

---
+++ boj/src/main/java/boj/Boj11050.java
@@ -0,0 +1,28 @@
+package boj;
+
+import java.util.Scanner;
+
+public class Boj11050 {
+    
+    public static int factorial(int num) {
+        int a = 1;
+
+        for(int i = 1; i <= num; i++) {
+            a *= i;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int bi = (factorial(n)) / (factorial(n - k) * factorial(k));
+        
+        System.out.println(bi);
+    }
+
+}
