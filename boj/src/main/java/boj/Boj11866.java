0e40468abc28982e0ee0f44a362850f649e4dee5
GraceNam <zeidgame@gmail.com>
2021-03-13 22:59:14 +0900

    docs : 요세푸스 문제 0
    + 백준 알고리즘 11866 solved
      + 덱을 사용하여 해결
      + 큐를 사용해서도 해결할 수 있지만 원형인 경우 덱이 좋다.

---
+++ boj/src/main/java/boj/Boj11866.java
@@ -0,0 +1,75 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Boj11866 {
+
+    /**
+     * 덱을 활용한 코드
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i < n + 1; i++) {
+            dq.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder("<");
+
+        while(!dq.isEmpty()) {
+            for(int i = 0; i < k - 1; i++) {
+                dq.addLast(dq.removeFirst());
+            }
+
+            sb.append(dq.removeFirst() + ", ");
+        }
+
+        System.out.println(sb.toString().substring(0, sb.length() - 2) + ">");
+
+    }
+
+    // 큐를 활용한 코드
+    // public static void main(String[] args) throws IOException {   
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    //     Queue<Integer> que = new LinkedList<>();
+
+    //     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    //     int n = Integer.parseInt(st.nextToken());
+    //     int k = Integer.parseInt(st.nextToken());
+
+    //     for(int i = 1; i < n + 1; i++) {
+    //         que.offer(i);
+    //     }
+
+    //     StringBuilder sb = new StringBuilder("<");
+
+    //     while(!que.isEmpty()) {
+    //         for(int i = 1; i < k + 1; i++) {
+    //             if(i != k) {
+    //                 que.offer(que.poll());
+    //             } else {
+    //                 sb.append(que.poll());
+    //                 sb.append(", ");
+    //             }
+    //         }
+    //     }
+
+    //     System.out.println(sb.toString().substring(0, sb.length() - 2) + ">");
+    // }
+    
+}
