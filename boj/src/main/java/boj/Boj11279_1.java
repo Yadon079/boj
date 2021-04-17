19ea3e088253161578d7493ae8c6c831dc6e285d
GraceNam <zeidgame@gmail.com>
2021-04-17 13:11:03 +0900

    docs : 최대 힙
    + 백준 11279 solved
      + 우선순위 큐
      + 11279_1은 최대 힙을 직접 구현

---
+++ boj/src/main/java/boj/Boj11279_1.java
@@ -0,0 +1,69 @@
+package boj;
+
+import java.io.*;
+
+public class Boj11279_1 {
+    
+    public static int [] heap = new int[100001];
+    public static int size = 0;
+    public static StringBuilder sb = new StringBuilder();
+ 
+    public static void swap(int x, int y) {
+        int temp = heap[x];
+        heap[x] = heap[y];
+        heap[y] = temp;
+    }
+    
+    public static void push(int x) {
+        heap[++size] = x;
+        
+        for(int i = size; i > 1; i /= 2) {
+            if(heap[i / 2] < heap[i]) {
+                swap(i / 2, i);
+            } else {
+                break;
+            }
+        }
+    }
+ 
+    public static void pop() {
+        sb.append(heap[1]+"\n");
+        heap[1] = heap[size];
+        heap[size--] = 0;
+        
+        for(int i = 1; i * 2 <= size; ) {
+            if(heap[i] > heap[i * 2] && heap[i] > heap[i * 2 + 1]) {
+                break;
+            } else if(heap[i * 2] > heap[i * 2 + 1]) {
+                swap(i, i * 2);
+                i = i * 2;
+            } else {
+                swap(i, i * 2 + 1);
+                i = i * 2 + 1;
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < n; i++) {
+            int k = Integer.parseInt(br.readLine());
+            if(k == 0) {
+                if(size == 0) {
+                    sb.append(0 + "\n");
+                } else {
+                    pop();
+                }
+            } else {
+                push(k);
+            }
+        }
+        
+        System.out.print(sb.toString());
+        
+        br.close();
+    }
+
+}
