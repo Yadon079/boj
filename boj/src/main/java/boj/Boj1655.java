9ebe8e96807f38c24cf671b3708b15431e93048c
GraceNam <zeidgame@gmail.com>
2021-04-18 22:21:46 +0900

    docs : 가운데를 말해요
    + 백준 1655 solved
      + 우선순위 큐

---
+++ boj/src/main/java/boj/Boj1655.java
@@ -0,0 +1,40 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj1655 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        PriorityQueue<Integer> min = new PriorityQueue<>();
+        PriorityQueue<Integer> max = new PriorityQueue<>(Comparator.reverseOrder());
+
+        int n = Integer.parseInt(br.readLine());        
+        int val = 0;
+
+        for(int i = 0; i < n; i++) {
+            val = Integer.parseInt(br.readLine());
+
+            if(max.size() <= min.size()) {
+                max.offer(val);
+            } else {
+                min.offer(val);
+            }
+
+            if(!min.isEmpty() && max.peek() > min.peek()) {
+                max.offer(min.poll());
+                min.offer(max.poll());
+            }
+
+            bw.write(max.peek() + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
