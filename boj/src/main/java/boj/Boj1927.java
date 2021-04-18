c1343607e66dbf75bcd98edf35ff5fcc583a2194
GraceNam <zeidgame@gmail.com>
2021-04-18 19:18:27 +0900

    docs : 최소 힙
    + 백준 1927 solved
      + 우선순위 큐

---
+++ boj/src/main/java/boj/Boj1927.java
@@ -0,0 +1,34 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj1927 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+        int val = 0;
+
+        for(int i = 0; i < n; i++) {
+            val = Integer.parseInt(br.readLine());
+
+            if(val != 0) {
+                que.add(val);
+            } else if(que.isEmpty()) {
+                bw.write("0\n");
+            } else {
+                bw.write(que.poll() + "\n");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
